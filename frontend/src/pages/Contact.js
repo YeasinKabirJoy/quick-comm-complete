4a94aec7c7c7275850bbdbc6ce42aa54bcc87655
Ahmad-Miraz <ahmad.miraz642@gmail.com>
2023-02-23 11:00:53 -0800

    frontend added

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,103 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import Container from "../components/Container";
+
+const Contact = () => {
+  return (
+    <>
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <Container class1="contact-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6144.220518035351!2d90.40651906173102!3d23.698285463362673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b90e998fbf79%3A0x600820ef3510caf7!2sKaliganj%2C%20Keraniganj!5e0!3m2!1sen!2sbd!4v1677042325678!5m2!1sen!2sbd"
+              width="600"
+              height="450"
+              className="border-0 w-100"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="col-12 mt-5">
+            <div className="contact-inner-wrapper d-flex justify-content-between ">
+              <div>
+                <h3 className="contact-title mb-4">Contact</h3>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                    />
+                  </div>
+                  <div>
+                    <textarea
+                      name=""
+                      id=""
+                      className="w-100 form-control"
+                      cols="30"
+                      rows="4"
+                      placeholder="Comments"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className="button border-0">Submit</button>
+                  </div>
+                </form>
+              </div>
+              <div>
+                <h3 className="contact-title mb-4">Get in touch with us</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineHome className="fs-5" />
+                      <address className="mb-0">
+                        Hno:East Chunkutia , South Keraniganj, Dhaka,
+                        Bangladesh
+                      </address>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
+                      <a href="tel:+91 8264954234">+880 1301972616</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineMail className="fs-5" />
+                      <a href="mailto:ahmad.miraz642@gmail.com">
+                      ahmad.miraz642@gmail.com
+                      </a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Contact;
---
+++ frontend/src/utils/Data.js
@@ -0,0 +1,28 @@
+export const services=[
+    {
+        title:"Free Shipping",
+        tagline:"From all orders over $5",
+        image:"images/service.png"
+    },
+    {
+        title:"Daily Surprise Offer",
+        tagline:"Save upto 25% OFF",
+        image:"images/service-02.png"
+    },
+    {
+        title:"Support 24/7",
+        tagline:"Shop With an Expert",
+        image:"images/service-03.png"
+    },
+    {
+        title:"Affordable Price",
+        tagline:"Get Factory Default Price",
+        image:"images/service-04.png"
+    },
+    {
+        title:"Secure Payment",
+        tagline:"100% Protected Payment",
+        image:"images/service-05.png"
+    },
+
+];
